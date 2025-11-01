6f1bc2f35fe0d19e42e049f6b42635b002070ad6
hsilva8086usp <hsilva8086@usp.br>
2025-11-01 11:13:02 -0300

    Caso de teste do semaforo de pedrestre utilizando k_uptime_get para calcular os intervalos.

--- src/main.c
+++ src/main.c
@@ -19,25 +19,49 @@ static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(LED_GREEN_NODE, gp
 
 void fn_thread_led_verde(){
 	while(1){
+
 		//Inicio da região crítica protegida
 		k_mutex_lock(&semaforo_mutex, K_FOREVER);
+
+		//k_uptime_get() para testar o tempo do LED
+		int64_t inicio = k_uptime_get();
+
 		// LED verde
 		gpio_pin_set_dt(&led_green, 1);
         k_msleep(LED_GREEN_NODE_SLEEP_TIME_MS);
 		gpio_pin_set_dt(&led_green, 0);
+
+		int64_t fim = k_uptime_get();
+
+
 		k_mutex_unlock(&semaforo_mutex);
+
+
+		printk("LED verde - %lld ms\n", fim - inicio);
 	}
 }
 
 void fn_thread_led_vermelho(){
 	while(1){
+
 		//Inicio da região crítica protegida
 		k_mutex_lock(&semaforo_mutex, K_FOREVER);
+
+
+		//k_uptime_get() para testar o tempo do LED
+		int64_t inicio = k_uptime_get();
+
 		// LED vermelho
 		gpio_pin_set_dt(&led_red, 1);
         k_msleep(LED_RED_NODE_SLEEP_TIME_MS);
 		gpio_pin_set_dt(&led_red, 0);
+		
+		int64_t fim = k_uptime_get();
+
 		k_mutex_unlock(&semaforo_mutex);
+
+
+		printk("LED vermelho - %lld ms\n", fim - inicio);
 	}
 }
 
